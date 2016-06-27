c3ce20ca8da9137250f756dfeb891409228485e2
Carter Fort <carter.fort@gmail.com>
2016-06-27 10:12:05 -0400

    DB Seeder. Use as you will to set up your own app

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,52 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $carter = factory(App\User::class)->create(['name' => 'Carter', 'email' => 'carter.fort@gmail.com']);
+        $christina = factory(App\User::class)->create(['name' => 'Christina', 'email' => 'christina.m.hoenig@gmail.com']);
+
+        $giantEagle = factory(App\ListCategory::class)->create(['name' => 'Giant Eagle', 'type' => 'Store']);
+        factory(App\ListCategory::class)->create(['name' => 'Lowes', 'type' => 'Store']);
+        factory(App\ListCategory::class)->create(['name' => 'Target', 'type' => 'Store']);
+
+        factory(App\ListCategory::class)->create(['name' => 'Grocery', 'type' => 'Type']);
+
+        $lists = factory(App\ListContainer::class, 5)->create(['title' => 'Shopping List']);
+        $lists->each(function($list) use($giantEagle) {
+        	$list->addItems( factory(App\ListItem::class, 10)->make(['list_id' => $list->id]) );
+	        $giantEagle->addList($list);
+        });
+
+
+        $bill = factory(App\Bill::class)->create(['entity' => "Duquesne Light", 'type' => 'Electricity', 'amount' => 10000, 'due_date' => '2016-08-01']);
+        $payment = factory(App\BillPayment::class)->make(['user_id' => $carter->id, 'amount' => 5000, 'payment_made_on' => '2016-07-20', 'bill_id' => $bill->id]);
+
+        factory(App\Chore::class)->create([
+            'name' => 'Load the dishwasher',
+            'summary' => 'Put any used dishes in the dishwasher. Hand-wash anything that needs to go in the drying rack',
+            'repeats_every' => null,
+            'best_time_to_do' => null
+        ]);
+
+        factory(App\Chore::class)->create([
+            'name' => 'Empty the dishwasher',
+            'summary' => 'Take the dishes out of the dish washer and put them into the cupboards',
+            'repeats_every' => null,
+            'best_time_to_do' => null
+        ]);
+
+        factory(App\Chore::class)->create([
+            'name' => 'Empty the kitchen bins',
+            'summary' => 'Take the garbage bags in the kitchen bins to the garage',
+            'repeats_every' => null,
+            'best_time_to_do' => null
+        ]);
+
+        factory(App\Chore::class)->create([
+            'name' => 'Take the garbage out to the curb',
+            'summary' => 'Take the garbage and recycling from the garage out to the curb',
+            'repeats_every' => "Thursday",
+            'best_time_to_do' => '17:00'
+        ]);
+
     }
 }
