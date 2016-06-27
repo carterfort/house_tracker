ed50fcafb98d5ca875b99e710698090347d0ed0f
Carter Fort <carter.fort@gmail.com>
2016-06-27 16:39:55 -0400

    Display error messages at the top

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,18 @@
 <body id="app-layout">
     
     @include('navbar')
-    
+
+    @if(count($errors) > 0)
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+
     <div class="container">
         @yield('main')
     </div>
