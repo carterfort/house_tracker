70fc80b23ea10e207b3ed6afa210746356937fab
Carter Fort <carter.fort@gmail.com>
2016-06-27 16:38:06 -0400

    Implement the dirtyAmounts trait
    Added a scope for recent bill payments for a given user (might not actually use)
    
    Added a madeBy() relationship
    
    Added a formatted amount for `amount_paid` attribute. Might refactor to a trait

--- app/BillPayment.php
+++ app/BillPayment.php
@@ -2,16 +2,43 @@
 
 namespace App;
 
+use Carbon\Carbon;
+use App\Billing\DirtyAmounts;
 use Illuminate\Database\Eloquent\Model;
 
 class BillPayment extends Model
 {
+
+	use DirtyAmounts;
+
     protected $dates = ['payment_made_at'];
 
-    protected $fillable = ['bill_id'];
-    
+    protected $fillable = ['bill_id', 'dirty_amount'];
+
+    public function scopeRecentForUser($query, $user)
+    {
+    	return $query->whereHas('madeBy', function($query) use ($user){
+    		$query->where('users.id', $user->id);
+    	})->where('created_at', '>', Carbon::now()->subWeeks(2));
+    }
+
     public function bill()
     {
     	return $this->belongsTo(Bill::class);
     }
+
+    public function madeBy()
+    {
+    	return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function getAmountPaidAttribute()
+    {
+    	return $this->formattedAmount($this->amount);
+    }
+
+    protected function formattedAmount($amount)
+	{
+		return '$'.number_format($amount / 100, 2, '.', ',');
+	}
 }
