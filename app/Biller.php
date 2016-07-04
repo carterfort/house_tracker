f00c4cb47550679fed0b311fe7e2b0b28fb2425a
Carter Fort <carter.fort@gmail.com>
2016-07-04 10:23:04 -0400

    Added scopes for finding billers that have or haven't been billed. Added the apportionment relationship

--- app/Biller.php
+++ app/Biller.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Biller extends Model
@@ -12,5 +13,41 @@ class Biller extends Model
     	'phone_number',
     	'website_url'
     ];
+
+    public function scopeAutoCreatesOnDay($query, $day)
+    {
+    	return $query->where('auto_creates', true)
+        ->where('auto_create_day', $day)
+        ->has('billsForThisMonth', 0);
+    }
+
+    public function billsForThisMonth()
+    {
+    	return $this->billsForMonth();
+    }
+
+    public function billsForMonth($month = false)
+    {
+    	$monthArray = startEndOfMonth($month);
+    	return $this->bills()->whereBetween('due_date', $monthArray);
+    }
+
+    public function createBillForThisMonth()
+    {
+    	Bill::create([
+    		'amount' => $this->auto_create_amount,
+    		'due_date' => Carbon::now()->endOfMonth(),
+    		'biller_id' => $this->id
+		]);
+    }
+
+    public function bills()
+    {
+    	return $this->hasMany(Bill::class);
+    }
     
+    public function apportionments()
+    {
+    	return $this->hasMany(BillApportionment::class);
+    }
 }
