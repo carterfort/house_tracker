a20a8df3b07d8bff7d785ce7b8f129e967b13982
Carter Fort <carter.fort@gmail.com>
2016-07-04 10:22:43 -0400

    Added a `validate()` method to make sure that each bill is fully apportioned

--- app/BillApportionment.php
+++ app/BillApportionment.php
@@ -3,17 +3,42 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Exceptions\InvalidApportionment;
 
 class BillApportionment extends Model
 {
+
+	public function biller()
+	{
+		return $this->belongsTo(Biller::class);
+	}
 
 	public function scopeBill($query, $bill)
 	{
 		return $query->where('biller_id', $bill->biller->id);
 	}
 
-    public function getMultiplierAttribute()
+	public function scopeUser($query, $user)
+	{
+		return $query->where('user_id', $user->id);
+	}
+
+    protected function getMultiplierAttribute()
     {
     	return $this->percentage / 100;
     }
+
+    public function amountDue($bill)
+	{
+		return $bill->amount * $this->multiplier;
+	}
+
+	public function validate()
+	{
+		$totalPercentageApportioned = ($this->biller->apportionments()->sum('percentage'));
+		if ($totalPercentageApportioned != 100)
+		{
+			throw new InvalidApportionment("Total percentage apportioned is {$totalPercentageApportioned}");
+		}
+	}
 }
