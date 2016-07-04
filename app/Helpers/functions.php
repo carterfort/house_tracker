13bee0a992542b4605212d7038abef6dcde4432b
Carter Fort <carter.fort@gmail.com>
2016-07-04 10:26:14 -0400

    Added `startEndOfMonth()` helper method for quickly getting an array of carbon instances that are the start and end of a given month. Defaults to the current month if no date is provided

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -1,7 +1,20 @@
 <?php
 
+use Carbon\Carbon;
 
 function home()
 {
 	return redirect()->route('home');
+}
+
+function startEndOfMonth($month){
+	
+	if ( ! $month) $month = Carbon::now();
+
+	if ( ! is_object($month)) $month = Carbon::parse($month);
+
+	$start = $month->copy()->startOfMonth();
+	$end = $start->copy()->endOfMonth();
+
+	return [$start, $end];
 }
