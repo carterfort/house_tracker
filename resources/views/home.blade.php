976576ee09d4aedab0408d535885d5693432e277
Carter Fort <carter.fort@gmail.com>
2016-07-23 09:13:34 -0400

    Display a user's bill obligations

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,50 +5,54 @@
 <h2>Hey there, {{auth()->user()->name}} <span class="glyphicon glyphicon-thumbs-up"></span></h2>
 
 <div class="row">
-    <div class="col-sm-3">
+    <div class="col-sm-4">
         <div class="panel panel-default">
             <div class="panel-heading">
-                <h3 class="panel-title">Bills</h3>
+                <h3 class="panel-title">Billers Missing Bills</h3>
             </div>
             <div class="panel-body">
-                <h5>Recently Paid</h5>
+            @if($billersWithoutBills->count())
                 <div class="list-group">
-                    @foreach(App\BillPayment::recentForUser(auth()->user())->get() as $payment)
+                @foreach($billersWithoutBills as $biller)
                     <div class="list-group-item">
-                        Paid {{$payment->amount_paid}} on 
-                        {{$payment->created_at->format('m/d/Y')}}
-                    </div>
-                    @endforeach
-                </div>
-                <h4>Due soon</h4>
-                <div class="list-group">
-
-                    @foreach(App\Bill::due() as $bill)
-                    <div class="list-group-item">
-                    <h5>
-                        {{$bill->biller->name}}
-                        <a href="/bills/{{$bill->id}}/payments/create">Pay bill
-                    </a>
-                    </h5>
-                    <hr />
-                    @foreach(App\User::all() as $user)
-                        @if($user->paidBill($bill))
-                        <span class="glyphicon glyphicon-check text-success"></span> 
-                        @else
-                        <span class="glyphicon glyphicon-remove text-danger"></span>
-                        @endif
-                        {{$user->name}} 
+                        {{ $biller->name }}
                         <br/>
-                    @endforeach
-
+                        <a href="/billers/{{$biller->id}}/bills/create" class="btn btn-xs btn-success btn-block">Add {{date('F')}} bill</a>
                     </div>
-                    @endforeach
+                @endforeach
                 </div>
+            @else
+                <small class="small"> <span class="glyphicon glyphicon-ok-sign text-success"></span> All billers accounted for</small>
+            @endif
             </div>
             <div class="panel-footer">
             </div>
         </div>
     </div>
+
+    <div class="col-sm-5">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">Unpaid Obligations</h3>
+            </div>
+            <div class="panel-body">
+                <div class="list-group">
+                @foreach($unpaidObligations as $obligation)
+                    <div class="list-group-item">
+
+                        {{ $obligation->formatted_amount }} for {{$obligation->bill->biller->name}} ({{$obligation->bill->biller->summary}})
+                        <br/>
+                        <a href="/obligations/{{$obligation->id}}/payments/create">Record payment</a>
+                    </div>
+                @endforeach
+                </div>
+            </div>
+            <div class="panel-footer">
+                Total obligations: {{displayCash($unpaidObligations->sum('amount'))}}
+            </div>
+        </div>
+    </div>
+
 </div>
 
 @endsection
